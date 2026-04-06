8f46e927aab01e5c29294cb49d49689f1a89d6d9
jordelmir <jordelmir@gmail.com>
2026-04-06 09:24:33 -0600

    feat(telemetry): Integrate Sentry for error tracking, session replay and performance monitoring

---
+++ sentry.edge.config.ts
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://0b37fe00d3e28e809f595cb10d538c58@o4510766099005440.ingest.us.sentry.io/4511173763465216",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
